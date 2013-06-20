1f5810c951406e24a230ed4816c5f2cc632a2b4c
Matt Jeanes <mattjeanes23@gmail.com>
2013-06-21 00:03:02 +0100

    Fixed
    Updated to new basecode.

--- lua/entities/wac_hc_roflcopter/shared.lua
+++ lua/entities/wac_hc_roflcopter/shared.lua
@@ -2,29 +2,29 @@ if not wac then return end
 
 ENT.Base 		= "wac_hc_base"
 ENT.Type 		= "anim"
-
 ENT.PrintName		= "RoflCopter"
 ENT.Author		= "Dr. Matt"
 ENT.Category		= wac.aircraft.spawnCategory
-ENT.Contact    		= ""
-ENT.Purpose 		= ""
-ENT.Instructions 	= ""
 ENT.Spawnable		= true
 ENT.AdminSpawnable	= true
 
 ENT.Model		= "models/drmatt/roflcopter/body.mdl"
-ENT.RotorPhModel	= "models/props_junk/sawblade001a.mdl"
-ENT.RotorModel	= "models/drmatt/roflcopter/mainrotor.mdl"
-ENT.BackRotorModel	= "models/drmatt/roflcopter/tailrotor.mdl"
-
-ENT.BackRotorDir	= -1
-ENT.TopRotorPos	= Vector(12.4,0,66)
-ENT.BackRotorPos	= Vector(-166,8,24)
 ENT.EngineForce	= 30
 ENT.Weight	= 9200
-
 ENT.SmokePos	= Vector(-40,0,30)
 ENT.FirePos	= Vector(-40,0,30)
+
+ENT.TopRotor = {
+	dir = -1,
+	pos = Vector(12.4,0,66),
+	model = "models/drmatt/roflcopter/mainrotor.mdl"
+}
+
+ENT.BackRotor = {
+	dir = -1,
+	pos = Vector(-166,8,24),
+	model = "models/drmatt/roflcopter/tailrotor.mdl"
+}
 
 ENT.Seats = {
 	{
